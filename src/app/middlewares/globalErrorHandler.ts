6c6d067636e18d554f713926c396de8947ea00aa
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-06-04 12:37:39 +0600

    Error handler

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,54 @@
+import { NextFunction, Request, Response } from 'express'
+import config from '../../config'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import handleValidationError from '../../errors/handlleValidationError'
+import ApiError from '../../errors/ApiError'
+
+const globalErrorHandler = (
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500
+  let message = 'Something went wrong !'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (err?.name === 'ValidatorError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode
+    message = err.message
+    errorMessages = err.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : []
+  } else if (err instanceof Error) {
+    message = err?.message
+    errorMessages = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : []
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  })
+  next()
+}
+
+export default globalErrorHandler
