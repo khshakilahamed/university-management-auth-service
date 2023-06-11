d840040041d9a63d638866a2205b0e0ba62c1929
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-06-12 01:44:38 +0600

    [done] academic department

---
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicDepartmentController } from './academicDepartment.controller';
+import { AcademicDepartmentValidation } from './academicDepartment.validation';
+const router = express.Router();
+
+router.post(
+  '/create-department',
+  validateRequest(
+    AcademicDepartmentValidation.createAcademicDepartmentZodSchema
+  ),
+  academicDepartmentController.createDepartment
+);
+
+router.get('/:id', academicDepartmentController.getSingleDepartment);
+router.get('/', academicDepartmentController.getAllDepartments);
+router.patch(
+  '/:id',
+  validateRequest(
+    AcademicDepartmentValidation.updateAcademicDepartmentZodSchema
+  ),
+  academicDepartmentController.updateDepartment
+);
+
+router.delete('/:id', academicDepartmentController.deleteDepartment);
+
+// export default route
+export const AcademicDepartmentRoutes = router;
